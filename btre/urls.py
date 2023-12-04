921876a3eaf68c451665a627ab67dbaf8fbe6567
ErnestaP <ernesta.petraityte@yahoo.com>
2023-12-04 21:14:21 +0100

    Accounts

--- btre/urls.py
+++ btre/urls.py
@@ -29,5 +29,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')), # empty, because we just want to have it in home page
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choice
 
 def index(request): #main listings page
     listings = Listing.objects.order_by('-list_date').filter(is_published=True) # objects.all() will fetch all the listings, order_by('-list_date') descending order (the most recent on top), for filter we are passing the name of our filed, and how we want to filter
@@ -20,4 +21,38 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    # keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords) #description__icontains; __icontains gives full text search in description
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city) #city__iexact; __iexact gives exact match in cities (i gives case insensitive, exact - case sensitive)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state) #city__iexact; __iexact gives exact match in cities (i gives case insensitive, exact - case sensitive)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms) #lte less or equal
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price) #lte less or equal
+
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choice': state_choice,
+        'listings': queryset_list,
+        'values': request.GET, # passing all the args from request,
+    }
+    return render(request, 'listings/search.html', context)
