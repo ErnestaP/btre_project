91722ba6e20cd28335d5be82757acc34cbdd8e85
ErnestaP <ernesta.petraityte@yahoo.com>
2023-12-28 14:44:53 +0000

    settings: added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -154,3 +154,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER= 'traversy.brad@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = ""
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
